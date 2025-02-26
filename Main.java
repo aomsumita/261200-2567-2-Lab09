160872d7819a71e8a5504bb729670e93f91d9edf
aomsumita <152596728+aomsumita@users.noreply.github.com>
2025-02-26 13:35:20 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import DIP.EmailNotifier;
+import DIP.Order;
+import DIP.OrderProcessor;
+import DIP.SMSNotifier;
+import LSP.Eagle;
+import LSP.Ostrich;
+import LSP.Penguin;
+import LSP.Sparrow;
+import OCP.Circle;
+import OCP.Ellipse;
+import OCP.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Rectangle R1 = new Rectangle(10,10);
+        System.out.println(R1.calculateArea());
+        Circle R2 = new Circle(5);
+        System.out.println(R2.calculateArea());
+        Ellipse R3 = new Ellipse(300,200);
+        System.out.println(R3.calculateArea());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Penguin pengu = new Penguin();
+        Sparrow sparro = new Sparrow();
+        Ostrich ostri = new Ostrich();
+        Eagle eag = new Eagle();
+
+        pengu.move();
+        sparro.move();
+        ostri.move();
+        eag.move();
+
+        sparro.fly();
+        eag.fly();
+
+        Order order = new Order();
+        OrderProcessor orderProcessor = new OrderProcessor(new EmailNotifier());
+        OrderProcessor orderProcessor2 = new OrderProcessor(new SMSNotifier());
+
+        orderProcessor.processOrder(order);
+        orderProcessor2.processOrder(order);
+
     }
 }
